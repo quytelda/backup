d55f904bea71212e8d093087b580dd0d1d2e726b
Quytelda Kahja <quytelda@tamalin.org>
2018-01-30 17:02:53 -0800

    Allow the user to specify source and location as command line parameters.

--- backup.sh
+++ backup.sh
@@ -7,19 +7,21 @@
 
 # BACKUPS is the destination directory where backups will be stored.
 # Edit BACKUPS to change the backup destination.
-BACKUPS=/backups
+BACKUPS=${1:-/backups}
 PREV_BACKUP=$BACKUPS/current
 CURR_BACKUP=$BACKUPS/backup-$(date "+%Y%m%d_%H%M%S")
 
 # SOURCE is the top level of the directory tree to back up.
 # Edit SOURCE to change the backup source.
-SOURCE=/
+SOURCE=${2:-/}
 
 # this script should be run as root
 if [ "$EUID" -ne 0 ]; then
    echo "This script must be run as root."
    exit
 fi
+
+echo "Backing up $SOURCE to $BACKUPS..."
 
 # do an incremental backup with rsync, skipping "special" files
 rsync -aAXP --link-dest=$PREV_BACKUP --exclude={"/dev","/proc","/sys","/tmp","/run","/mnt","/media","/lost+found"} $SOURCE $CURR_BACKUP
